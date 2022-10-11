105da8a0ae4f01673c17252e3e65ec20e7f313ee
arajguha <anurajguha@gmail.com>
2022-10-11 22:02:17 +0530

    initial commit

---
+++ src/main/java/app/hook/archival/ArchivalApplication.java
@@ -0,0 +1,13 @@
+package app.hook.archival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivalApplication.class, args);
+	}
+
+}
